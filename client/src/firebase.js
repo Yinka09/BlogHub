0598ca3020ca7d6b92de842d5a66c7ad41d6d612
Yinka09 <kadirimaranatha@yahoo.com>
2024-07-06 04:46:36 +0100

    BlogHub

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,18 +1,41 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+// import { initializeApp } from 'firebase/app';
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: 'mern-blog-b327f.firebaseapp.com',
+//   projectId: 'mern-blog-b327f',
+//   storageBucket: 'mern-blog-b327f.appspot.com',
+//   messagingSenderId: '699397991367',
+//   appId: '1:699397991367:web:88ff565ef72a182d6b87e2',
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-blog-b327f.firebaseapp.com',
-  projectId: 'mern-blog-b327f',
-  storageBucket: 'mern-blog-b327f.appspot.com',
-  messagingSenderId: '699397991367',
-  appId: '1:699397991367:web:88ff565ef72a182d6b87e2',
+  authDomain: "alx-blogging-app.firebaseapp.com",
+  projectId: "alx-blogging-app",
+  storageBucket: "alx-blogging-app.appspot.com",
+  messagingSenderId: "702248665119",
+  appId: "1:702248665119:web:79ea2afd36e2e5653a552a",
+  measurementId: "G-6S6FPCT1R0",
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+
+// const analytics = getAnalytics(app);
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,31 +1,35 @@
 export default function About() {
   return (
-    <div className='min-h-screen flex items-center justify-center'>
-      <div className='max-w-2xl mx-auto p-3 text-center'>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
         <div>
-          <h1 className='text-3xl font font-semibold text-center my-7'>
-            About Sahand' Blog
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            About BlogHub
           </h1>
-          <div className='text-md text-gray-500 flex flex-col gap-6'>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
             <p>
-              Welcome to Sahand's Blog! This blog was created by Sahand Ghavidel
-              as a personal project to share his thoughts and ideas with the
-              world. Sahand is a passionate developer who loves to write about
-              technology, coding, and everything in between.
+              BlogHub is a dynamic and user-friendly blogging platform built
+              using the MERN stack (MongoDB, Express.js, React, and Node.js).
+              Designed for bloggers and readers alike, BlogHub offers a seamless
+              experience for content creation, management, and consumption. Our
+              platform leverages JWT for secure authentication, ensuring that
+              users' data and interactions are safe and protected.
             </p>
 
             <p>
-              On this blog, you'll find weekly articles and tutorials on topics
-              such as web development, software engineering, and programming
-              languages. Sahand is always learning and exploring new
-              technologies, so be sure to check back often for new content!
+              BlogHub features an intuitive interface that makes it easy to
+              write, edit, and publish blog posts. Readers can enjoy real-time
+              updates, rich content, and a responsive design that looks great on
+              any device. Our goal is to create a community where ideas and
+              stories can be shared effortlessly, fostering engagement and
+              connection among users.
             </p>
 
             <p>
-              We encourage you to leave comments on our posts and engage with
-              other readers. You can like other people's comments and reply to
-              them as well. We believe that a community of learners can help
-              each other grow and improve.
+              Whether you are an avid blogger or a casual reader, BlogHub
+              provides the tools and features you need to stay connected and
+              inspired. Join us on BlogHub and be a part of a vibrant and
+              growing community of content creators and enthusiasts.
             </p>
           </div>
         </div>
